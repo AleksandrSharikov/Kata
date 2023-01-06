77a2680313db9a92c008ba93887faa34e2561435
AleksandrSharikov <Aleksandr.N.Sharikov@gmail.com>
2023-01-07 01:47:50 +0300

    First commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +18,99 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        Statement st = Util.getStatement();
+        try {
+            st.execute("CREATE TABLE IF NOT EXISTS USERS " +
+                    "(" +
+                    "ID INT AUTO_INCREMENT," +
+                    " name VARCHAR(50)," +
+                    " LastName VARCHAR(50)," +
+                    " AGE INT," +
+                    " primary key (ID)" +
+                    ")");
+            st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
+        Statement st = Util.getStatement();
+        try {
+            st.execute("DROP TABLE IF EXISTS USERS; ");
+         //   st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
+        Statement st = Util.getStatement();
+        try {
+            st.executeUpdate("INSERT INTO USERS (NAME,LASTNAME,AGE) " +
+                    "VALUES ('" + name + "','" + lastName + "','" + age + "')");
+         //   st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     public void removeUserById(long id) {
+        Statement st = Util.getStatement();
+        try {
+            st.executeUpdate("DELETE FROM USERS WHERE id='" + id + "'");
+          //  st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+
+        Statement st = Util.getStatement();
+        {
+
+            ResultSet resultSet;
+            try {
+                resultSet = st.executeQuery("SELECT * FROM USERS");
+
+                while (resultSet.next()) {
+
+                    long id = resultSet.getLong("ID");
+                    String name = resultSet.getString("NAME");
+                    String lastName = resultSet.getString("LASTNAME");
+                    byte age = resultSet.getByte("AGE");
+
+                    User user = new User();
+                    user.setId(id);
+                    user.setName(name);
+                    user.setLastName(lastName);
+                    user.setAge(age);
+
+                    userList.add(user);
+                }
+            //    st.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            return userList;
+        }
     }
 
     public void cleanUsersTable() {
+        Statement st = Util.getStatement();
+        try {
+            st.executeUpdate("TRUNCATE USERS");
+         //   st.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String name = "root";
+    private static final String password = "root";
+    private static final String URL = "jdbc:mysql://localhost/test";
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    public static Statement getStatement() {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Connection connection = DriverManager.getConnection(URL, name, password);
+            return connection.createStatement();
+            //statement.executeUpdate("CREATE TABLE IF NOT EXISTS test2 (id int primary key auto_increment, name VARCHAR(10) NOT NULL); ");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
