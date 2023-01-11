01004805a9b20c69639748b3c4f56f1f1bbb6844
AleksandrSharikov <Aleksandr.N.Sharikov@gmail.com>
2023-01-11 16:47:36 +0300

    Delete some garbage

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,6 +3,7 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import java.util.ArrayList;
@@ -16,18 +17,24 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
+        SessionFactory sf = Util.getSessionFactory();
+        Session session = sf.openSession();
         Transaction tx = session.beginTransaction();
-        session.createSQLQuery("CREATE TABLE IF NOT EXISTS USERS " +
-                "(" +
-                "ID INT AUTO_INCREMENT," +
-                " name VARCHAR(50)," +
-                " LastName VARCHAR(50)," +
-                " AGE INT," +
-                " primary key (ID)" +
-                ")").executeUpdate();
-        tx.commit();
-        session.close();
+        try {
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS USERS " +
+                    "(" +
+                    "ID INT AUTO_INCREMENT," +
+                    " name VARCHAR(50)," +
+                    " LastName VARCHAR(50)," +
+                    " AGE INT," +
+                    " primary key (ID)" +
+                    ")").executeUpdate();
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            session.close();
+        }
 
     }
 
@@ -35,9 +42,14 @@ public class UserDaoHibernateImpl implements UserDao {
     public void dropUsersTable() {
         Session session = Util.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
+        try {
         session.createSQLQuery("DROP TABLE USERS").executeUpdate();
         tx.commit();
-        session.close();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            session.close();
+        }
 
     }
 
@@ -45,34 +57,49 @@ public class UserDaoHibernateImpl implements UserDao {
     public void saveUser(String name, String lastName, byte age) {
         Session session = Util.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
+        try {
         session.persist(new User(name,lastName,age));
         tx.commit();
-        session.close();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
         Session session = Util.getSessionFactory().openSession();
         Transaction tx = session.beginTransaction();
-        if(session.get(User.class, id) != null) {
-            session.remove(session.get(User.class, id));
+        User user;
+        try {
+        if((user = session.get(User.class, id)) != null) {
+            session.remove(user);
         }
         tx.commit();
-        session.close();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-
-        return (ArrayList<User>)  Util.getSessionFactory().openSession().createQuery("From User").list();
+        return (ArrayList<User>) Util.getSessionFactory().openSession().createQuery("From User").list();
     }
 
     @Override
     public void cleanUsersTable() {
         Session session = Util.getSessionFactory().openSession();
         Transaction  tx = session.beginTransaction();
+        try {
         session.createQuery("DELETE User").executeUpdate();
         tx.commit();
-        session.close();
+        }catch (Exception e) {
+            tx.rollback();
+        }finally {
+            session.close();
+        }
     }
 }
